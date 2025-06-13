d9c4d6e48cba2f6508de709244f706bfacbe6ae2
Hangil Chang <hangil2010@naver.com>
2025-06-13 10:36:18 +0900

    Feat: Enemy Spawner
    소환된 적이 맵 내 MovePoint 태그를 검색하여 추적하는 방식으로 변경
    
    이유 : 프리팹화 할 경우 씬 뷰 내의 이동 포인트를 인식하지 못하는 문제가 발생.

--- Assets/RandomSpawnPoint.cs
+++ Assets/RandomSpawnPoint.cs
@@ -7,9 +7,14 @@ public class RandomSpawnPoint : MonoBehaviour
     [Header("References")]
     [SerializeField] Transform _startPos;
     [SerializeField] Transform _endPos;
-
     void Start()
     {
-        float _randomSpawnPoint = Mathf.Round(Random.Range(_startPos.position.x, _endPos.position.x));
+        float _randomSpawnPoint = Random.Range(_startPos.position.x, _endPos.position.x);
+        Debug.Log(_randomSpawnPoint);
+    }
+
+    void Update()
+    {
+
     }
 }
--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -1,20 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Data;
+using TMPro;
 using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour
 {
-    /// <summary>
-    /// NavMesh를 사용하는 것도 좋을 거 같으나, 맵 내 포인트로 이동하게 하는 것이 더 좋다고 판단.
-    /// </summary>
     // Start is called before the first frame update
 
-    [Header("References")]
-    [SerializeField] private Transform _movePoint;
+    
     [Header("Atteributes")]
     [SerializeField] private float _moveSpeed = 5f;
 
+    private Transform _movePoint;
     private Rigidbody _rb;
     void Start()
     {
@@ -23,6 +21,8 @@ public class EnemyMovement : MonoBehaviour
 
     void FixedUpdate()
     {
+        _movePoint = GameObject.FindWithTag("MovePoint").transform;
+
         if (_movePoint != null)
         {
             Vector3 direction = (_movePoint.position - transform.position).normalized;
